c457662813f46558ba880aee7d56b2b475a28153
rafiattaui <athallah.rafi13@gmail.com>
2026-06-19 15:29:01 +0700

    add: fcfs scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from process import Process
 
 class BaseScheduler(ABC):
 
@@ -13,4 +14,24 @@ class BaseScheduler(ABC):
     @abstractmethod
     def is_empty(self):
         pass
+
+class FCFS(BaseScheduler):
+    # first come first serve
+    # new processes are added to the end,
+    # the process at the front of the queue is selected for execution
+
+    def __init__(self):
+        self.readyqueue = []
+    
+    def select_next(self) -> Process:
+        return self.readyqueue.pop()
+
+    def add_process(self, process: Process):
+        self.readyqueue.append(process)
+
+    def is_empty(self) -> bool:
+        return bool(self.readyqueue)
+    
+    def __repr__(self):
+        return f"FCFS Scheduler, Ready Queue: {self.readyqueue}"
 
